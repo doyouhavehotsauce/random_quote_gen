51fd94342b91ee0a9304bc1c8b528ac88d97d5ab
Robert Crozier <robertcrozier@Roberts-MacBook-Air.local>
2019-12-16 15:42:36 -0500

    random quote func working

--- random-quote-gen/js/script.js
+++ random-quote-gen/js/script.js
@@ -13,43 +13,53 @@ project 1 - A Random Quote Generator
 let quotes = [
   { quote: 'I\'m killing time while I wait for life to shower me with meaning and happiness.', 
     author: 'Bill Watterson', 
-    source: 'Calvin and Hobbes'
+    citation: 'Calvin and Hobbes',
+    year: 1988
   },
   { quote: 'You know what\'s weird? Day by day, nothing seems to change, but pretty soon...everything\'s different', 
     author: 'Bill Watterson', 
-    source: 'Calvin and Hobbes'
+    citation: 'Calvin and Hobbes',
+    year: 1989
   },
   { quote: 'You’ve got to be very careful if you don’t know where you are going, because you might not get there', 
-  author: 'Yogi Berra', 
-  source: ''
+    author: 'Yogi Berra', 
+    citation: 'Source Unknown',
+    year: 1973
   },
   { quote: 'You’ll miss the best things if you keep your eyes shut. Sometimes the questions are complicated and the answers are simple.', 
-  author: 'Dr. Seuss', 
-  source: ''
+    author: 'Dr. Seuss', 
+    citation: 'Source Unknown',
+    year: 1964
   },
   { quote: 'This must be Thursday,\' said Arthur to himself, sinking low over his beer. \'I never could get the hang of Thursdays.', 
-  author: 'Douglas Adams', 
-  source: 'The Hitchhikers Guide to the Galaxy'
+    author: 'Douglas Adams', 
+    citation: 'The Hitchhikers Guide to the Galaxy',
+    year: 1978
   },
   { quote: 'I never trust people with no appetite. It’s like they’re always holding something back on you.', 
-  author: 'Haruki Murakami', 
-  source: 'Hard-Boiled Wonderland and the End of the World'
+    author: 'Haruki Murakami', 
+    citation: 'Hard-Boiled Wonderland and the End of the World',
+    year: 2004
   },
   { quote: 'Sometimes you wake up. Sometimes the fall kills you. And sometimes, when you fall, you fly.', 
-  author: 'Neil Gaiman', 
-  source: 'The Sandman, Vol. 6: Fables and Reflections'
+    author: 'Neil Gaiman', 
+    citation: 'The Sandman, Vol. 6: Fables and Reflections',
+    year: 2006
   },
   { quote: 'Laugh hard, it’s a long way to the bank.', 
-  author: 'Modest Mouse', 
-  source: 'Paper Thin Walls'
+    author: 'Modest Mouse', 
+    citation: 'Paper Thin Walls',
+    year: 2002
   },
   { quote: 'I am and always will be the optimist. The hoper of far-flung hopes and the dreamer of improbable dreams.', 
-  author: 'Dr. Who', 
-  source: 'Season 6, Episode 6 - \'The Almost People\''
+    author: 'Dr. Who', 
+    citation: 'Season 6, Episode 6 - \'The Almost People\'',
+    year: 1982
   },
-  { quote: 'We live in an age when unnecessary things are our only necessities.', 
-  author: 'Oscar Wilde', 
-  source: ''
+  { quote: 'I am and always will be the optimist. The hoper of far-flung hopes and the dreamer of improbable dreams.', 
+    author: 'Dr. Who', 
+    citation: 'Season 6, Episode 6 - \'The Almost People\'',
+    year: 1982
   }
 
 ]
@@ -58,13 +68,35 @@ let quotes = [
 /***
  * `getRandomQuote` function
 ***/
+let randomQuote = '';
+let quoteAuthor = '';
+let quoteCitation = '';
+let quoteYear;
+let randomness;
 
+function getRandomQuote(){
+//Get a random number between 1 & 10
+  const randomNum = Math.floor(Math.random() * (quotes.length) ) ;
+
+  randomQuote = quotes[randomNum].quote;
+  quoteAuthor = quotes[randomNum].author;
+  quoteCitation = quotes[randomNum].citation;
+  quoteYear = quotes[randomNum].year;
+} 
+
+getRandomQuote();
+// console.log(randomQuote);
+// console.log(quoteAuthor);
+// console.log(quoteCitation);
+// console.log(quoteYear);
 
 
 /***
  * `printQuote` function
 ***/
-
+function printQuote(){
+  randomness = getRandomQuote();
+}
 
 
 /***
