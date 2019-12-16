aa30b63529f80afbd296178091369f4dcc06f66f
Robert Crozier <robertcrozier@Roberts-MacBook-Air.local>
2019-12-16 06:52:55 -0500

    quotes in place

--- random-quote-gen/js/script.js
+++ random-quote-gen/js/script.js
@@ -10,7 +10,49 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
+let quotes = [
+  { quote: 'I\'m killing time while I wait for life to shower me with meaning and happiness.', 
+    author: 'Bill Watterson', 
+    source: 'Calvin and Hobbes'
+  },
+  { quote: 'You know what\'s weird? Day by day, nothing seems to change, but pretty soon...everything\'s different', 
+    author: 'Bill Watterson', 
+    source: 'Calvin and Hobbes'
+  },
+  { quote: 'You’ve got to be very careful if you don’t know where you are going, because you might not get there', 
+  author: 'Yogi Berra', 
+  source: ''
+  },
+  { quote: 'You’ll miss the best things if you keep your eyes shut. Sometimes the questions are complicated and the answers are simple.', 
+  author: 'Dr. Seuss', 
+  source: ''
+  },
+  { quote: 'This must be Thursday,\' said Arthur to himself, sinking low over his beer. \'I never could get the hang of Thursdays.', 
+  author: 'Douglas Adams', 
+  source: 'The Hitchhikers Guide to the Galaxy'
+  },
+  { quote: 'I never trust people with no appetite. It’s like they’re always holding something back on you.', 
+  author: 'Haruki Murakami', 
+  source: 'Hard-Boiled Wonderland and the End of the World'
+  },
+  { quote: 'Sometimes you wake up. Sometimes the fall kills you. And sometimes, when you fall, you fly.', 
+  author: 'Neil Gaiman', 
+  source: 'The Sandman, Vol. 6: Fables and Reflections'
+  },
+  { quote: 'Laugh hard, it’s a long way to the bank.', 
+  author: 'Modest Mouse', 
+  source: 'Paper Thin Walls'
+  },
+  { quote: 'I am and always will be the optimist. The hoper of far-flung hopes and the dreamer of improbable dreams.', 
+  author: 'Dr. Who', 
+  source: 'Season 6, Episode 6 - \'The Almost People\''
+  },
+  { quote: 'We live in an age when unnecessary things are our only necessities.', 
+  author: 'Oscar Wilde', 
+  source: ''
+  }
 
+]
 
 
 /***
